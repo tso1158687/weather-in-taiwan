8974230c568a1462add84544ca886baefc1526cf
jason <tso1158687@gmail.com>
2020-04-20 01:29:41 +0800

    add env setting and weather service

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  cwbApiUrl: 'https://opendata.cwb.gov.tw/api/v1/rest/datastore/ß',
+  cwbAccessKey: 'CWB-1711E839-6B6F-4528-933D-6E520E6FE59D'
 };
 
 /*
