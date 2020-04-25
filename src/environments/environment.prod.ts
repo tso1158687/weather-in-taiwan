650e8071fada8b9c3fc087c66a774fef6fe349e7
jason <jason.cheng@lileesystems.com>
2020-04-25 13:31:52 +0800

    add angular cli package

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  cwbApiUrl: 'https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-D0047-091',
+  cwbAccessKey: 'CWB-1711E839-6B6F-4528-933D-6E520E6FE59D'
 };
