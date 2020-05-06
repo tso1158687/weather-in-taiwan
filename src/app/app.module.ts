0fb887ccb62d5c4bb91fc2fde447a7162a6cefc0
jason <jason.cheng@lileesystems.com>
2020-05-06 15:46:01 +0800

    add toolbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule }    from '@angular/common/http';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+const materialModule=[MatToolbarModule,MatIconModule]
 @NgModule({
   declarations: [
     AppComponent
@@ -13,7 +16,8 @@ import { HttpClientModule }    from '@angular/common/http';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    HttpClientModule
+    HttpClientModule,
+    ...materialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
